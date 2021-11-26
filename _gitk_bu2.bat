e314121ee8eadab16a626fa51b22de209ded38c7
Gabor Liptak <liptakgabor@freemail.hu>
2021-11-26 15:16:24 +0100

    bu2 gitk script is adapted for git 2.29.2

--- _gitk_bu2.bat
+++ _gitk_bu2.bat
@@ -1,3 +1,5 @@
-@set path=C:\KBApps\DevEnv\cygwin\Git\bin\;%PATH%
+::@set path=C:\KBApps\DevEnv\cygwin\Git\bin\;%PATH%
+@set path=C:\KBApps\DevEnv\Git.2.29.2\cmd;%PATH%
 @set HOME=C:\Users\Liptak
-gitk
+::gitk
+git-gui.exe
